d4ef60b4787e1300bde30c192a292b06e5535f26
Alexey Stolbov <alstoff@live.ru>
2025-04-16 17:45:47 +0300

    HW_22_скорректирована форма contacts.html

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'catalog/index.html')
+
+def catalog_view(request):
+    return render(request, 'catalog/catalog.html')
+
+def category_view(request):
+    return render(request, 'catalog/category.html')
+
+def contacts_view(request):
+    if request.method == "POST":
+        name = request.POST.get("name", "Без имени")
+        email = request.POST.get("email", "Без почты")
+        message = request.POST.get("message", "Без сообщения")
+
+        print("Новое сообщение:")
+        print(f"Имя: {name}")
+        print(f"Email: {email}")
+        print(f"Сообщение: {message}")
+
+        return render(request, 'catalog/contacts.html', {
+            "success": True,
+            "name": name
+        })
+
+    return render(request, 'catalog/contacts.html')

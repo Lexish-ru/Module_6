9969a790dad88db64b7a2bc92601dd0b169ba525
Alexey Stolbov <alstoff@live.ru>
2025-04-29 20:40:24 +0300

    HW_24_исправлен views

--- catalog/views.py
+++ catalog/views.py
@@ -1,38 +1,39 @@
-from django.shortcuts import render
-from .models import Message, Product
-from .forms import MessageForm
+# catalog/views.py
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
 
-def home(request):
-    products = Product.objects.all()
-    return render(request, 'catalog/index.html', {'products': products})
+def home_view(request):
+    return render(request, "catalog/index.html")
+
 
 def catalog_view(request):
-    return render(request, 'catalog/catalog.html')
+    products = Product.objects.all()
+    return render(request, "catalog/catalog.html", {"products": products})
 
-def category_view(request):
-    return render(request, 'catalog/category.html')
 
-def contacts_view(request):
-    form = MessageForm(request.POST or None)
-
-    if request.method == "POST" and form.is_valid():
-        form.save()
-        return render(request, 'catalog/contacts.html', {
-            "success": True,
-            "name": form.cleaned_data.get("name"),
-            "form": MessageForm()  # новая пустая форма после отправки
-        })
-
-    return render(request, 'catalog/contacts.html', {
-        "form": form
+def category_view(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    products = Product.objects.filter(category=category)
+    return render(request, "catalog/category.html", {
+        "category": category,
+        "products": products
     })
 
-def messages_view(request):
-    messages = Message.objects.order_by('-created_at')
-    return render(request, 'catalog/messages.html', {'messages': messages})
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, "catalog/category_list.html", {"categories": categories})
 
 
 def product_detail(request, pk):
-    product = Product.objects.get(pk=pk)
-    return render(request, 'catalog/product.html', {'product': product})
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, "catalog/product.html", {"product": product})
+
+
+def contacts_view(request):
+    return render(request, "catalog/contacts.html")
+
+
+def messages_view(request):
+    return render(request, "catalog/messages.html")

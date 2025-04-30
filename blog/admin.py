94a22f8c2bc34d65787d6f2c4ab92c1b652ff98d
Alexey Stolbov <alstoff@live.ru>
2025-04-30 15:41:26 +0300

    HW_25_модель BlogPost зарегистрирована в admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import BlogPost
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "is_published", "created_at", "views_count")
+    list_filter = ("is_published", "created_at")
+    search_fields = ("title", "content")
+    readonly_fields = ("views_count", "created_at", "updated_at")

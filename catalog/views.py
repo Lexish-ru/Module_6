b2435f0f3bd92da8a1ecd1d5b52c9cb66f614425
Alexey Stolbov <alstoff@live.ru>
2025-05-05 17:10:18 +0300

    HW_24_структура каталога вынесена в base.html

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,3 @@
-# catalog/views.py
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 from .forms import MessageForm
@@ -9,10 +8,14 @@ def home_view(request):
     return render(request, "catalog/index.html", {"products": products})
 
 
-
 def catalog_view(request):
     products = Product.objects.all()
     return render(request, "catalog/catalog.html", {"products": products})
+
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, "catalog/category.html", {"categories": categories})
 
 
 def category_view(request, pk):
@@ -24,19 +27,10 @@ def category_view(request, pk):
     })
 
 
-def category_list(request):
-    categories = Category.objects.all()
-    return render(request, "catalog/category.html", {"categories": categories})
-
-
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     return render(request, "catalog/product.html", {"product": product})
 
-
-def contacts_view(request):
-    form = MessageForm()
-    return render(request, "catalog/contacts.html", {"form": form})
 
 def contacts_view(request):
     success = False
@@ -47,15 +41,15 @@ def contacts_view(request):
             message = form.save()
             success = True
             name = message.name
-            form = MessageForm()  # сбрасываем форму
+            form = MessageForm()
     else:
         form = MessageForm()
-
     return render(request, "catalog/contacts.html", {
         "form": form,
         "success": success,
         "name": name
     })
+
 
 def messages_view(request):
     return render(request, "catalog/messages.html")

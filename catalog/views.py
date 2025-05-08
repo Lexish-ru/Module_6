fe9fed74ad3512ce18cdde0b39262bf145339d5a
Alexey Stolbov <alstoff@live.ru>
2025-05-08 14:07:15 +0300

    HW_25_выполнен перенос каталога на CBV

--- catalog/views.py
+++ catalog/views.py
@@ -1,61 +1,44 @@
-# catalog/views.py
-from django.shortcuts import render, get_object_or_404
+from django.views.generic import TemplateView, ListView, DetailView, FormView
+from django.shortcuts import get_object_or_404
 from .models import Product, Category
 from .forms import MessageForm
+from django.urls import reverse_lazy
 
+class HomeView(ListView):
+    model = Product
+    template_name = "catalog/index.html"
+    context_object_name = "products"
 
-def home_view(request):
-    products = Product.objects.all()
-    return render(request, "catalog/index.html", {"products": products})
+class CatalogView(ListView):
+    model = Product
+    template_name = "catalog/catalog.html"
+    context_object_name = "products"
 
+class CategoryDetailView(DetailView):
+    model = Category
+    template_name = "catalog/category.html"
+    context_object_name = "category"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["products"] = Product.objects.filter(category=self.object)
+        return context
 
-def catalog_view(request):
-    products = Product.objects.all()
-    return render(request, "catalog/catalog.html", {"products": products})
+class CategoryListView(ListView):
+    model = Category
+    template_name = "catalog/category.html"
+    context_object_name = "categories"
 
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "catalog/product.html"
+    context_object_name = "product"
 
-def category_view(request, pk):
-    category = get_object_or_404(Category, pk=pk)
-    products = Product.objects.filter(category=category)
-    return render(request, "catalog/category.html", {
-        "category": category,
-        "products": products
-    })
+class ContactsView(FormView):
+    template_name = "catalog/contacts.html"
+    form_class = MessageForm
+    success_url = reverse_lazy("contacts")
 
-
-def category_list(request):
-    categories = Category.objects.all()
-    return render(request, "catalog/category.html", {"categories": categories})
-
-
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    return render(request, "catalog/product.html", {"product": product})
-
-
-def contacts_view(request):
-    form = MessageForm()
-    return render(request, "catalog/contacts.html", {"form": form})
-
-def contacts_view(request):
-    success = False
-    name = ""
-    if request.method == "POST":
-        form = MessageForm(request.POST)
-        if form.is_valid():
-            message = form.save()
-            success = True
-            name = message.name
-            form = MessageForm()  # сбрасываем форму
-    else:
-        form = MessageForm()
-
-    return render(request, "catalog/contacts.html", {
-        "form": form,
-        "success": success,
-        "name": name
-    })
-
-def messages_view(request):
-    return render(request, "catalog/messages.html")
+    def form_valid(self, form):
+        self.object = form.save()
+        return self.render_to_response(self.get_context_data(success=True, name=self.object.name, form=self.form_class()))

604cbd9fa918d070116b454a030a3a5c67b862c6
Alexey Stolbov <alstoff@live.ru>
2025-05-08 14:06:05 +0300

    HW_25_отредактированы представления

--- blog/admin.py
+++ blog/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import BlogPost
+from .models import Post
 
-@admin.register(BlogPost)
+@admin.register(Post)
 class BlogPostAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "is_published", "created_at", "views_count")
     list_filter = ("is_published", "created_at")
--- blog/views.py
+++ blog/views.py
@@ -4,40 +4,42 @@ from .models import Post
 from .forms import PostForm
 
 def post_list(request):
-    posts = Post.objects.all()
-    return render(request, "post_list.html", {"posts": posts})
+    posts = Post.objects.order_by('-created_at')
+    return render(request, "blog/post_list.html", {"posts": posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, "post_detail.html", {"post": post})
+    post.views += 1
+    post.save(update_fields=["views"])
+    return render(request, "blog/post_detail.html", {"post": post})
 
 def post_create(request):
     if request.method == "POST":
-        form = PostForm(request.POST)
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             messages.success(request, "Пост успешно создан.")
             return redirect("post_list")
     else:
         form = PostForm()
-    return render(request, "post_form.html", {"form": form})
+    return render(request, "blog/post_form.html", {"form": form})
 
 def post_update(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
+        form = PostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
             form.save()
-            messages.success(request, "Пост успешно обновлен.")
+            messages.success(request, "Пост успешно обновлён.")
             return redirect("post_detail", pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, "post_form.html", {"form": form})
+    return render(request, "blog/post_form.html", {"form": form})
 
 def post_delete(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         post.delete()
-        messages.success(request, "Пост удален.")
+        messages.success(request, "Пост удалён.")
         return redirect("post_list")
-    return render(request, "post_confirm_delete.html", {"post": post})
+    return render(request, "blog/post_confirm_delete.html", {"post": post})

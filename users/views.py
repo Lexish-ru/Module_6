7861e7feda8b268c7e3dcf8f25d248821405b3bf
Alexey Stolbov <alstoff@live.ru>
2025-05-22 16:39:06 +0300

    HW_27_создана страница авторизации

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import login
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from .forms import UserRegisterForm
@@ -7,4 +8,10 @@ class RegisterView(CreateView):
     model = User
     form_class = UserRegisterForm
     template_name = 'users/register.html'
-    success_url = reverse_lazy('login')  # или 'catalog'
+    success_url = reverse_lazy('catalog')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)  # Автоматический вход
+        return response
+

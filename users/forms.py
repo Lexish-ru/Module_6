b626c4d7ed806c2b6faad461ccf5b7f1a015b224
Alexey Stolbov <alstoff@live.ru>
2025-05-26 00:40:36 +0300

    HW_27_ОБНОВЛЕНЫ ФОРМЫ

--- users/forms.py
+++ users/forms.py
@@ -6,3 +6,14 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'country', 'phone', 'avatar']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'

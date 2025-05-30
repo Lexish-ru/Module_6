1f636833cf3f6d94eddf8bc48029c6d8407e6f19
Alexey Stolbov <alstoff@live.ru>
2025-05-30 10:43:05 +0300

    HW_29_К проекту подключен Redis

--- config/settings.py
+++ config/settings.py
@@ -149,5 +149,16 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            # "PASSWORD": "если есть пароль, добавь сюда"
+        }
+    }
+}
 
+CACHE_ENABLED = True
 

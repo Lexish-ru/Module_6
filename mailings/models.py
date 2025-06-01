8cf82b2ba68fd5ae918b943ea0f9d2d0175bbf44
Alexey Stolbov <alstoff@live.ru>
2025-06-01 22:39:27 +0300

    CP_5_определена модель Client

--- mailings/models.py
+++ mailings/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    email = models.EmailField(unique=True, verbose_name="E-mail")
+    full_name = models.CharField(max_length=150, verbose_name="ФИО")
+    comment = models.TextField(blank=True, verbose_name="Комментарий")
+
+    def __str__(self):
+        return f"{self.full_name} <{self.email}>"

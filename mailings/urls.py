97c17bd4445ae6342a50cd92b0d0359305c230d9
Alexey Stolbov <alstoff@live.ru>
2025-06-02 15:13:17 +0300

    CP_5_обновлены маршруты

--- mailings/urls.py
+++ mailings/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('clients/', views.ClientListView.as_view(), name='client-list'),
+    path('clients/add/', views.ClientCreateView.as_view(), name='client-add'),
+    path('clients/<int:pk>/edit/', views.ClientUpdateView.as_view(), name='client-edit'),
+    path('clients/<int:pk>/delete/', views.ClientDeleteView.as_view(), name='client-delete'),
 ]

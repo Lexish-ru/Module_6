fb726d5537c15cf455bf1f79bf560e947664bae3
Alexey Stolbov <alstoff@live.ru>
2025-06-02 16:56:05 +0300

    CP_5_обновлены маршруты

--- mailings/urls.py
+++ mailings/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('clients/add/', views.ClientCreateView.as_view(), name='client-add'),
     path('clients/<int:pk>/edit/', views.ClientUpdateView.as_view(), name='client-edit'),
     path('clients/<int:pk>/delete/', views.ClientDeleteView.as_view(), name='client-delete'),
+    path('messages/', views.MessageListView.as_view(), name='message-list'),
+    path('messages/add/', views.MessageCreateView.as_view(), name='message-add'),
+    path('messages/<int:pk>/edit/', views.MessageUpdateView.as_view(), name='message-edit'),
+    path('messages/<int:pk>/delete/', views.MessageDeleteView.as_view(), name='message-delete'),
 ]

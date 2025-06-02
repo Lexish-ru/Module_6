20927d8276c9989cb85b5e3b9471ede463b147e3
Alexey Stolbov <alstoff@live.ru>
2025-06-02 14:58:09 +0300

    CP_5_реализованы views

--- mailings/views.py
+++ mailings/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Client
 
 # Create your views here.
 
 def home(request):
     return render(request, 'mailings/home.html')
+
+class ClientListView(ListView):
+    model = Client
+    template_name = 'mailings/client_list.html'
+
+class ClientCreateView(CreateView):
+    model = Client
+    fields = ['email', 'full_name', 'comment']
+    template_name = 'mailings/client_form.html'
+    success_url = reverse_lazy('client-list')
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    fields = ['email', 'full_name', 'comment']
+    template_name = 'mailings/client_form.html'
+    success_url = reverse_lazy('client-list')
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    template_name = 'mailings/client_confirm_delete.html'
+    success_url = reverse_lazy('client-list')

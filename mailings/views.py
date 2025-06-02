da3639d32e86e49fee3ae25ea492ef4241ce14a9
Alexey Stolbov <alstoff@live.ru>
2025-06-02 16:49:47 +0300

    CP_5_созданы views для модели Message

--- mailings/views.py
+++ mailings/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from .models import Client
-from .forms import ClientForm
+from .models import Client, Message
+from .forms import ClientForm, MessageForm
 
 # Create your views here.
 
@@ -29,3 +29,24 @@ class ClientDeleteView(DeleteView):
     model = Client
     template_name = 'mailings/client_confirm_delete.html'
     success_url = reverse_lazy('client-list')
+
+class MessageListView(ListView):
+    model = Message
+    template_name = 'mailings/message_list.html'
+
+class MessageCreateView(CreateView):
+    model = Message
+    form_class = MessageForm
+    template_name = 'mailings/message_form.html'
+    success_url = reverse_lazy('message-list')
+
+class MessageUpdateView(UpdateView):
+    model = Message
+    form_class = MessageForm
+    template_name = 'mailings/message_form.html'
+    success_url = reverse_lazy('message-list')
+
+class MessageDeleteView(DeleteView):
+    model = Message
+    template_name = 'mailings/message_confirm_delete.html'
+    success_url = reverse_lazy('message-list')

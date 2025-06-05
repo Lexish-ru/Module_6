b1b32a3f401d24c6f474af2f2007095aaaa713de
Alexey Stolbov <alstoff@live.ru>
2025-06-05 11:27:56 +0300

    CP_5_создано новое приложение USers

--- mailing_service/settings.py
+++ mailing_service/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = env('EMAIL_HOST')
 EMAIL_PORT = env.int('EMAIL_PORT')
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')

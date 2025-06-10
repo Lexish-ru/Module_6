b9c2ffcf963214aa8ac0a4c1179673bcbb1f746f
Alexey Stolbov <alstoff@live.ru>
2025-06-10 22:31:29 +0300

    CP_5_прописаны маршруты

--- mailing_service/urls.py
+++ mailing_service/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mailings.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('users/', include('users.urls')),
+
 ]

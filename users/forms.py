6a88675a998ba1feccb838dca6c44a863d2450ac
Alexey Stolbov <alstoff@live.ru>
2025-06-16 00:26:45 +0300

    CP_прописаны докстринги в forms пользователя

--- users/forms.py
+++ users/forms.py
@@ -4,11 +4,15 @@ from .models import CustomUser
 
 
 class ProfileForm(forms.ModelForm):
+    """Форма для редактирования профиля пользователя."""
     class Meta:
         model = CustomUser
         fields = ['email', 'first_name', 'last_name', 'avatar', 'phone', 'country']
 
     def __init__(self, *args, **kwargs):
+        """
+        Добавляет класс Bootstrap к полям формы.
+        """
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control'

2699eff080c764811b2c9d8f34fe6e1295b44ac9
Alexey Stolbov <alstoff@live.ru>
2025-06-16 00:28:11 +0300

    CP_прописаны докстринги в admin пользователя

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
 from .models import CustomUser
-
-# Register your models here.
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
+    """
+    Админка для кастомной модели пользователя.
+    """
     model = CustomUser
     list_display = ('email', 'first_name', 'last_name', 'is_staff')
     fieldsets = (

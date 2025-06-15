da25217565c65662eed008b0f00bb1327a9ae3aa
Alexey Stolbov <alstoff@live.ru>
2025-06-16 00:22:06 +0300

    CP_прописаны докстринги в admin  рассылки

--- mailings/admin.py
+++ mailings/admin.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
+from .models import Client, Message, Mailing, MailingAttempt
 
-from .models import Client, Mailing, MailingAttempt, Message
+class ClientAdmin(admin.ModelAdmin):
+    """Админка для клиентов рассылки."""
+    list_display = ('email', 'full_name', 'owner')
+    search_fields = ('email', 'full_name')
+    list_filter = ('owner',)
 
-admin.site.register(Client)
-admin.site.register(Message)
-admin.site.register(Mailing)
-admin.site.register(MailingAttempt)
+class MessageAdmin(admin.ModelAdmin):
+    """Админка для сообщений рассылки."""
+    list_display = ('subject', 'owner')
+    search_fields = ('subject',)
+    list_filter = ('owner',)
+
+class MailingAdmin(admin.ModelAdmin):
+    """Админка для рассылок."""
+    list_display = ('id', 'status', 'start_at', 'end_at', 'owner')
+    search_fields = ('id',)
+    list_filter = ('status', 'owner')
+
+class MailingAttemptAdmin(admin.ModelAdmin):
+    """Админка для попыток рассылки."""
+    list_display = ('id', 'mailing', 'client', 'attempted_at', 'status', 'owner')
+    search_fields = ('id', 'mailing__id', 'client__email')
+    list_filter = ('status', 'owner')
+
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Mailing, MailingAdmin)
+admin.site.register(MailingAttempt, MailingAttemptAdmin)
